1f6bd733ba489e04bb978116bd15f7bb95c295b0
pramuddhika <pramuddhika21@yahoo.com>
2023-10-23 06:43:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/testcofee4/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.testcofee4;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.testcofee4.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragement(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.home) {
+                replaceFragement(new HomeFragment());
+            } else if (item.getItemId() == R.id.menu) {
+                replaceFragement(new MenuFragment());
+            } else if (item.getItemId() == R.id.cart) {
+                replaceFragement(new CartFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragement(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+}
