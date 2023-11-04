dff5dcd013fae63b9a47cca01bbc6603ba613ad1
pramuddhika <pramuddhika21@yahoo.com>
2023-11-04 06:14:00 +0530

    change starting activity

---
+++ app/src/main/java/com/example/testcofee4/FristPage.java
@@ -0,0 +1,32 @@
+package com.example.testcofee4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class FristPage extends AppCompatActivity {
+
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frist_page);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(FristPage.this, Loading1.class );
+                startActivity(intent);
+                finish();
+
+            }
+        },1000);
+    }
+}
